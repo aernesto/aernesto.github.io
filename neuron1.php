5cb6041b08f1a1e8e056072ebe347ceb1b16e8fd
adrian <adrian.radillo@gmail.com>
2017-03-25 20:07:25 -0500

    initial commit

---
+++ neuron1.php
@@ -0,0 +1,40 @@
+<?php include("/export/web-main/www/users/adrian/www/password_protect.php"); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>UH - Department of Mathematics</title>
+  <link href="math_style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="wrapper">
+<div id="wrapper_1">
+<div id="left_logo"><a href="http://www.mathematics.uh.edu/"><img
+ src="math_logo.png" alt="University of Houston" height="80" width="400"></a></div>
+<div id="right_uhlink"> </div>
+<div class="clear"> </div>
+</div>
+<div id="wrapper_2"><!-- Menu Bar starts here -->
+<ul>
+  <li><a href="index.html">HOME</a></li>
+  <li><a href="teaching.html">TEACHING</a></li>
+  <li><a href="research.html">RESEARCH</a></li>
+  <li><a href="CV_Radillo.pdf">CV</a></li>
+  <li><a href="resources.html">RESOURCES</a></li>
+</ul>
+<!-- Menu Bar ends here --> </div>
+<div id="wrapper_5"><!-- Begin Edit Content here -->
+<h1><strong>Resources</strong></h1>
+this is the page for neuron1<br>
+</div>
+</div>
+<!-- End Content  -->
+<div id="wrapper_footer">
+<div id="footercontainer">
+<div id="wrapper_7"><a href="http://www.mathematics.uh.edu/">Department
+of Mathematics</a>, 651 PGH, Houston, Texas 77204-3008 - Voice:
+713-743-3500 - Fax: 713-743-3505</div>
+</div>
+</div>
+</body>
+</html>
